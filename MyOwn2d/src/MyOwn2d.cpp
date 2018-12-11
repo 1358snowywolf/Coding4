81456749e5a26556e724e6894394ebc486052148
test account <abc@example.com>
2018-12-10 20:05:23 -0800

    12/10/2018

--- MyOwn2d/src/MyOwn2d.cpp
+++ MyOwn2d/src/MyOwn2d.cpp
@@ -49,6 +49,9 @@ void fbuild(int L, int R, int p, vector<vector<int>> initialSeg, vector<vector<i
 }
 
 int fquerry(int p, int L, int R, int j1, int j2, int i, vector<vector<int>>& finalSeg, int type){
+
+//	cout << p << "  " << j1 << "  " << j2 << "   " << L << "  " << R << endl;
+
 	if(L >= j1 && R <= j2){
 		return finalSeg[i][p];
 	}
@@ -60,6 +63,15 @@ int fquerry(int p, int L, int R, int j1, int j2, int i, vector<vector<int>>& fin
 	int p2 = fquerry(right(p), ((L + R) / 2) + 1, R, j1, j2, i, finalSeg, type);
 
 	if(type == 1){
+//		cout << p << "  " << j1 << "  " << j2 << "   " << L << "  " << R << "  " << i << endl;
+//		cout << "size " << finalSeg[i].size() << endl;
+//		for (auto x: finalSeg[i]) {
+//			cout << x << "  ";
+//		}
+//		cout << endl;
+//
+//		cout << "max : " << max(p1, p2) << endl;
+//		cout << " ----  " << endl;
 		return max(p1, p2);
 	}
 
@@ -67,6 +79,7 @@ int fquerry(int p, int L, int R, int j1, int j2, int i, vector<vector<int>>& fin
 }
 
 int querry(int p, int i1, int i2, int j1, int j2, int L, int R, vector<vector<int>>& finalSeg, int type){
+//	cout << p << "  " << i1 << "  " << i2 << "   " << L << "  " << R << endl;
 	if(L >= i1 && R <= i2){
 		return fquerry(1, 0, N - 1, j1, j2, p, finalSeg, type);
 	}
@@ -84,35 +97,41 @@ int querry(int p, int i1, int i2, int j1, int j2, int L, int R, vector<vector<in
 	return min(p1, p2);
 }
 
-void fupdate(int p, int L, int R, int i, int j, int val, vector<vector<int>>& finalSeg, int type){
+void fupdate(int p, int L, int R, int row, int j, int val, vector<vector<int>>& finalSeg, int type){
 	if(L > R) return;
 	if(L == R){
-		finalSeg[i][p] = val;
+//		finalSeg[p][L] = val;
+//		cout << row << " " << p << endl;
+		finalSeg[row][p] = val;
 		return;
 	}
 
 	if(L <= j && j <= (L + R) / 2){
-		fupdate(left(p), L, (L + R) / 2, i, j, val, finalSeg, type);
+		fupdate(left(p), L, (L + R) / 2, row, j, val, finalSeg, type);
 	}
 	else{
-		fupdate(right(p), ((L + R) / 2) + 1, R, i, j, val, finalSeg, type);
+		fupdate(right(p), ((L + R) / 2) + 1, R, row, j, val, finalSeg, type);
 	}
 
 	if(type == 1){
-		finalSeg[i][p] = max(finalSeg[i][left(p)], finalSeg[i][right(p)]);
+		finalSeg[row][p] = max(finalSeg[row][left(p)], finalSeg[row][right(p)]);
 	}
 	else{
-		finalSeg[i][p] = min(finalSeg[i][left(p)], finalSeg[i][right(p)]);
+		finalSeg[row][p] = min(finalSeg[row][left(p)], finalSeg[row][right(p)]);
 	}
 }
 
 void update(int p, int L, int R, int i, int j, int val, vector<vector<int>>& finalSeg, int type){
+//	cout << i << " " << j << " " << L << " " << R << endl;
+//	cout << "----------------------" << endl;
+
 	if(L == R){
-		fupdate(1, 0, N - 1, L, j, val, finalSeg, type);
+//		cout << L << " " << R << " " << p << " " << i << endl;
+		fupdate(1, 0, N - 1, p, j, val, finalSeg, type);
 		return;
 	}
 
-	if(L <= j && j <= (L + R) / 2){
+	if(L <= i && i <= (L + R) / 2){
 		update(left(p), L, (L + R) / 2, i, j, val, finalSeg, type);
 	}
 	else{
@@ -121,10 +140,10 @@ void update(int p, int L, int R, int i, int j, int val, vector<vector<int>>& fin
 
 	for(int k = 0; k < (int) finalSeg[i].size(); k++){
 		if(type == 1){
-			finalSeg[i][p] = max(finalSeg[i][left(p)], finalSeg[i][right(p)]);
+			finalSeg[p][k] = max(finalSeg[left(p)][k], finalSeg[right(p)][k]);
 		}
 		else{
-			finalSeg[i][p] = min(finalSeg[i][left(p)], finalSeg[i][right(p)]);
+			finalSeg[p][k] = min(finalSeg[left(p)][k], finalSeg[right(p)][k]);
 		}
 	}
 }
@@ -152,10 +171,10 @@ int main() {
 //		cout << endl;
 //	}
 
-	vector<vector<int>> maxInitialSeg (N * 2, vector<int> (N * 2, 0));
+	vector<vector<int>> maxInitialSeg (N * 4, vector<int> (N * 4, 0));
 	vector<vector<int>> maxFinalSeg (N * 4, vector<int> (N * 4, 0));
 
-	vector<vector<int>> minInitialSeg (N * 2, vector<int> (N * 2, 2000000));
+	vector<vector<int>> minInitialSeg (N * 4, vector<int> (N * 4, 2000000));
 	vector<vector<int>> minFinalSeg (N * 4, vector<int> (N * 4, 2000000));
 
 	for(int i = 0; i < N; i++){
@@ -169,16 +188,79 @@ int main() {
 	int Q;
 	cin >> Q;
 
-	cout << endl;
+//	cout << endl;
+////
+////	for(int i = 0; i < 7; i++){
+////		for(int j = 0; j < 7; j++){
+////			if(minFinalSeg[i][j] == 2000000){
+////				cout << "X ";
+////			}
+////			else{
+////				cout << minFinalSeg[i][j] << " ";
+////			}
+////		}
+////		cout << endl;
+////	}
+////
+////	cout << endl << endl;
+////
+////	for(int i = 0; i < 7; i++){
+////		for(int j = 0; j < 7; j++){
+////			if(maxFinalSeg[i][j] == 2000000){
+////				cout << "X ";
+////			}
+////			else{
+////				cout << maxFinalSeg[i][j] << " ";
+////			}
+////		}
+////		cout << endl;
+////	}
+////
+////	cout << endl;
+//
+//
+//	for(int i = 0; i < N * 3; i++){
+//		for(int j = 0; j < N * 3; j++){
+//			if(minFinalSeg[i][j] == 2000000){
+//				cout << "X ";
+//			}
+//			else{
+//				cout << minFinalSeg[i][j] << " ";
+//			}
+//		}
+//		cout << endl;
+//	}
+//
+//	cout << endl;
+//	cout << endl;
 
-	for(int i = 0; i < 12; i++){
-		for(int j = 0; j < 12; j++){
-			cout << maxFinalSeg[i][j] << " ";
-		}
-		cout << endl;
-	}
-
-	cout << endl;;
+//	for(int i = 0; i < 12; i++){
+//		for(int j = 0; j < 20; j++){
+//			if(minFinalSeg[i][j] == 2000000){
+//				cout << "X ";
+//			}
+//			else{
+//				cout << minFinalSeg[i][j] << " ";
+//			}
+//		}
+//		cout << endl;
+//	}
+//
+//	cout << endl << endl;
+//
+//	for(int i = 0; i < 12; i++){
+//		for(int j = 0; j < 20; j++){
+//			if(maxFinalSeg[i][j] == 2000000){
+//				cout << "X ";
+//			}
+//			else{
+//				cout << maxFinalSeg[i][j] << " ";
+//			}
+//		}
+//		cout << endl;
+//	}
+//
+//	cout << endl;
 
 	for(int a = 0; a < Q; a++){
 		char command;
@@ -198,8 +280,66 @@ int main() {
 			i--; j--;
 
 			//update(int p, int L, int R, int i, int j, int val, vector<vector<int>>& finalSeg, int type)
+
 			update(1, 0, N - 1, i, j, val, maxFinalSeg, 1);
 			update(1, 0, N - 1, i, j, val, minFinalSeg, 2);
+
+//			maps[i][j] = val;
+//
+//			for(int k = 0; k < N; k++){
+//				build(0, N - 1, 1, k, minInitialSeg, 2);
+//				build(0, N - 1, 1, k, maxInitialSeg, 1);
+//			}
+//
+//			fbuild(0, N - 1, 1, minInitialSeg, minFinalSeg, 2);
+//			fbuild(0, N - 1, 1, maxInitialSeg, maxFinalSeg, 1);
+
+//			cout << endl;
+//
+//			for(int i = 0; i < N * 3; i++){
+//				for(int j = 0; j < N * 3; j++){
+//					if(minFinalSeg[i][j] == 2000000){
+//						cout << "X ";
+//					}
+//					else{
+//						cout << minFinalSeg[i][j] << " ";
+//					}
+//				}
+//				cout << endl;
+//			}
+////
+//			cout << endl;
+
+
+//			cout << endl;
+//
+//			for(int i = 0; i < 7; i++){
+//				for(int j = 0; j < 7; j++){
+//					if(minFinalSeg[i][j] == 2000000){
+//						cout << "X ";
+//					}
+//					else{
+//						cout << minFinalSeg[i][j] << " ";
+//					}
+//				}
+//				cout << endl;
+//			}
+//
+//			cout << endl << endl;
+//
+//			for(int i = 0; i < 7; i++){
+//				for(int j = 0; j < 7; j++){
+//					if(maxFinalSeg[i][j] == 2000000){
+//						cout << "X ";
+//					}
+//					else{
+//						cout << maxFinalSeg[i][j] << " ";
+//					}
+//				}
+//				cout << endl;
+//			}
+//
+//			cout << endl;
 		}
 	}
 
